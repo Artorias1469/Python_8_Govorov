2d595d96a111abc7e05d96a2d6fdd5b25954898f
Artorias1469 <pupas5676@gmail.com>
2023-11-30 22:45:19 +0300

    ffkfkf

---
+++ code/individ1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def print_elements_after_duplicate_pair(tup):
+    if len(tup) < 2:
+        return
+
+    if tup[0] == tup[1]:
+        print(tup[2:])
+    else:
+        print_elements_after_duplicate_pair(tup[1:])
+
+if __name__ == "__main__":
+    my_tuple = (1, 2, 3, 4, 4, 5, 6, 7, 7, 8)
+    print_elements_after_duplicate_pair(my_tuple)
